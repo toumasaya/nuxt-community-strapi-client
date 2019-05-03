6876f644dfcf4844eba064ddec2c454a204e4960
Salt <luffy.w.pirate@gmail.com>
2019-05-03 14:19:35 +0800

    Add switch theme mode function

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -11,10 +11,35 @@
               icon-pack="fas"
               icon="search")
         div(class="navbar-item")
-          div(class="field")
-            b-switch(:value="true") Dark Mode
+          div(class="field theme-switch")
+            b-switch(
+              v-model="themeMode"
+              true-value="Dark Mode",
+              false-value="Light Mode"
+              @change.native="setThemeMode") {{ themeMode }}
         div(class="navbar-item")
           div(class="buttons")
             nuxt-link(class="button is-primary" to="/"): strong Sign up
             nuxt-link(class="button" to="/") Sign in
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      themeMode: 'Dark Mode'
+    }
+  },
+  methods: {
+    setThemeMode() {
+      const mode = this.themeMode
+
+      if (mode === 'Dark Mode') {
+        document.documentElement.setAttribute('data-theme', 'dark')
+      } else if (mode === 'Light Mode') {
+        document.documentElement.setAttribute('data-theme', 'light')
+      }
+    }
+  }
+}
+</script>
