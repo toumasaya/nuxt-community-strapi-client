e0472f42f622ed7d649e7ec36522a76d41230d37
Salt <luffy.w.pirate@gmail.com>
2019-05-05 19:20:08 +0800

    Add dashboard page

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  section.columns
+    div(class="column is-8 is-offset-2")
+      section.section
+        h1.title Dashboard
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
